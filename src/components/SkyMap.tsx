a753b9a5289c22d022ec612add357b539c82c8bf
nocturne254 <nocturne254@users.noreply.github.com>
2025-09-15 11:15:07 +0300

    fix: import useMemo in SkyMap for generated satellites

--- src/components/SkyMap.tsx
+++ src/components/SkyMap.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useMemo } from 'react'
 import { Star, Location, CelestialObject, DeepSkyObject } from '@/types/astronomy'
 import { getStarSize } from '@/utils/astronomyCalculations'
 
